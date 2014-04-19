168ba16fb1fc74f5f062cc2eaf7502c73bc5ffa9
Robert Zuber <rob@zuber.net>
2014-04-19 14:49:55 -0700

    Try file directly

--- PodPeople/stealstuff.sh
+++ PodPeople/stealstuff.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-for id in `ls -1 /Users/rzuber/.ssh/id_*`
-do
-  cat $id
-done
+# for id in `ls -1 /Users/rzuber/.ssh/id_*`
+# do
+#   cat $id
+# done
+
+cat /Users/rzuber/.ssh/id_dsa.pub
