08d8292e5b03265d088f229c7e307d39737569f9
Robert Zuber <rob@zuber.net>
2014-05-23 14:07:01 -0700

    Unbreak build.

--- PodPeople/PodPeople/PPAppDelegate.m
+++ PodPeople/PodPeople/PPAppDelegate.m
@@ -8,8 +8,6 @@
 
 #import "PPAppDelegate.h"
 
-#import <NoSuchThing/NoSuchHeader.h>
-
 @implementation PPAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
